c7d60f324cc58a48074c723da5af8a21d4aacdc0
Welzewool <blackenedima@yandex.ru>
2025-04-01 19:43:39 +0300

    добавлена стартовая команда

--- bot/handlers/basic_commands.py
+++ bot/handlers/basic_commands.py
@@ -1,23 +1,24 @@
 from aiogram.types import Message
 from bot.bot import TelegramBot
 import logging
+from aiogram import Router, types
 from aiogram import Dispatcher, types
 from aiogram.filters import Command
 from sqlalchemy.future import select
 # from bot.database.models import User
 # from bot.database.database import async_session
-# from bot.keyboards.reply import get_start_keyboard
+from bot.keyboards.main_menu import main_menu_keyboard
 
 
+router = Router()
+
+@router.message(Command("start"))
 async def cmd_start(message: Message):
-    await message.answer("Привет! Я бот для учета финансов. Используй /help для списка команд.")
-
-async def cmd_help(message: Message):
-    pass
-
-
-def register_handlers_basic_commands(dp: Dispatcher) -> None:
-    """  """
-    dp.message.register(cmd_start, Command("start"))
-
+    await message.answer("Добро пожаловать! Я бот для учета финансов 💰.\n\n"
+                         "Доступные команды:\n"
+                         "/add_income - Добавить доход\n"
+                         "/add_expense - Добавить расход\n"
+                         "/history - Показать историю транзакций\n",
+                         reply_markup=main_menu_keyboard()
+                         )
 
