9c7289c3f31a9f0ed9f88fdf93d42f5c6ac83c6e
Welzewool <blackenedima@yandex.ru>
2025-04-01 19:42:56 +0300

    поправлены импорты

--- bot/models/__init__.py
+++ bot/models/__init__.py
@@ -1,5 +1,6 @@
 from .user import User
-from .transactions import Transactions
 from .category import Category
+from .transactions import Transactions
 
-__all__ = ["User", "Transactions", "Category"]
+
+__all__ = ["User", "Category", "Transactions"]
