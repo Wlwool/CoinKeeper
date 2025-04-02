51672782807a533aa66877d6bf6495dc479a5043
Welzewool <blackenedima@yandex.ru>
2025-04-02 17:58:38 +0300

    исправлено название таблицы на categories

--- bot/models/category.py
+++ bot/models/category.py
@@ -1,16 +1,18 @@
 from sqlalchemy import Column, Integer, Float, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from bot.database.database import Base
+from datetime import datetime
 
 
 class Category(Base):
     """
     Модель таблицы категории расходов
     """
-    __tablename__ = 'category'
-    id = Column(Integer, primary_key=True, index=True)
-    name = Column(String(100))
+    __tablename__ = 'categories'
+    id = Column(Integer, primary_key=True)
     user_id = Column(Integer, ForeignKey('users.id'))
+    name = Column(String(50), nullable=False)
+    type = Column(String(10), nullable=False)  # 'income' или 'expense'
+    created_at = Column(DateTime, default=datetime.now)
 
-    user = relationship("User", back_populates="category")
-
+    user = relationship("User", back_populates="categories")
