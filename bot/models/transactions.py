880f1bf0838326d02a000b88e0a7843659fc56dc
Welzewool <blackenedima@yandex.ru>
2025-04-02 18:03:21 +0300

    исправлена ошибка, вместо user_tg_id было просто id

--- bot/models/transactions.py
+++ bot/models/transactions.py
@@ -7,14 +7,12 @@ class Transactions(Base):
     """
     Модель транзакций пользователей
     """
-
     __tablename__ = 'transactions'
-
     id = Column(Integer, primary_key=True, autoincrement=True)
     amount = Column(Float, nullable=False)
+    category = Column(String, nullable=False)
     type = Column(String, nullable=False)
     date = Column(DateTime, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.id'))
+    user_id = Column(Integer, ForeignKey('users.user_tg_id'))  # Изменено на user_tg_id
     price = Column(Float, nullable=False)
-
     user = relationship('User', back_populates='transactions')
