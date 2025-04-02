2901f9d42c3221f59dfd337462db5617c801d9ac
Welzewool <blackenedima@yandex.ru>
2025-04-02 17:59:44 +0300

    функция gte_session переделана под асинхронный контекстный менеджер

--- bot/database/database.py
+++ bot/database/database.py
@@ -4,6 +4,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker
 from bot.config.config import Config
 from bot.database.base import Base
+from contextlib import asynccontextmanager
 
 
 logger = logging.getLogger(__name__)
@@ -59,14 +60,28 @@ async def setup_db():
         logger.error(f"Критическая ошибка при создании таблиц: {e}")
         raise
 
-async def get_session() -> AsyncSession:
-    """Генератор асинхронных сессий для работы с базой данных."""
-    async with async_session() as session:
-        try:
-            yield session
-        except Exception as e:
-            logger.error(f"Ошибка при работе с сессией БД: {e}")
-            await session.rollback()
-            raise
-        finally:
-            await session.close()
+@asynccontextmanager
+async def get_session():
+    """Асинхронный контекстный менеджер для работы с базой данных."""
+    session = async_session()
+    try:
+        yield session
+        await session.commit()
+    except Exception as e:
+        logger.error(f"Ошибка при работе с сессией БД: {e}")
+        await session.rollback()
+        raise
+    finally:
+        await session.close()
+
+# async def get_session() -> AsyncSession:
+#     """Генератор асинхронных сессий для работы с базой данных."""
+#     async with async_session() as session:
+#         try:
+#             yield session
+#         except Exception as e:
+#             logger.error(f"Ошибка при работе с сессией БД: {e}")
+#             await session.rollback()
+#             raise
+#         finally:
+#             await session.close()
