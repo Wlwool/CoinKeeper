48c44b96b862caecf2c42be9c81f24ae10825779
Welzewool <blackenedima@yandex.ru>
2025-04-04 18:55:13 +0300

    добавлен хэндлер для обработка всего, что не попадает в другие обработчики

---
+++ bot/handlers/unknown_handler.py
@@ -0,0 +1,20 @@
+from aiogram import Router, F
+from aiogram.types import Message
+import logging
+from bot.keyboards.main_menu import main_menu_keyboard
+
+router = Router()
+logger = logging.getLogger(__name__)
+
+
+@router.message(F.text)
+async def unknown_message(message: Message) -> None:
+    """
+    Обработка всего что не попадает в другие обработчики
+    :param message:
+    """
+    logger.warning(f"Неизвестный ввод {message.from_user.id}: {message.text}")
+    await message.answer(
+        "Я не понимаю эту команду. Пожалуйста, используйте кнопки меню или команды.",
+        reply_markup=main_menu_keyboard()
+    )
