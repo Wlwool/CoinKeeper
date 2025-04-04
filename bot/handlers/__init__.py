d8883253e38a382b73320dd533db55cfaa66d1b9
Welzewool <blackenedima@yandex.ru>
2025-04-04 18:51:57 +0300

    добавлены импорты для новых роутеров history_router и unknown_router

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,11 +1,14 @@
 from aiogram import Router
 from .basic_commands import router as basic_router
 from .transactions import router as transactions_router
+from .transaction_history import router as history_router
+from .unknown_handler import router as unknown_router
 
 main_router = Router()
 
 main_router.include_router(basic_router)
 main_router.include_router(transactions_router)
-
+main_router.include_router(history_router)
+main_router.include_router(unknown_router)
 
 __all__ = ["main_router"]
