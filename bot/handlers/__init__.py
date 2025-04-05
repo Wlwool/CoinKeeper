cf570f3abc89bc25f555bb710720ab95a4f803ec
Welzewool <blackenedima@yandex.ru>
2025-04-05 18:36:18 +0300

    добавлен отдельный роутер для отмены действий

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -1,4 +1,5 @@
 from aiogram import Router
+from .cancel_handler import router as cancel_router
 from .basic_commands import router as basic_router
 from .transactions import router as transactions_router
 from .transaction_history import router as history_router
@@ -6,6 +7,7 @@ from .unknown_handler import router as unknown_router
 
 main_router = Router()
 
+main_router.include_router(cancel_router)
 main_router.include_router(basic_router)
 main_router.include_router(transactions_router)
 main_router.include_router(history_router)
