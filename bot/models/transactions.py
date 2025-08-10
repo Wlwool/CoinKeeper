0ad8d4e16d7aa768c6523429344a146e54940df9
Wlwool <blackenedima@yandex.ru>
2025-08-10 13:02:10 +0300

    добавлено описание и убрано дублирующее поле цены

--- bot/models/transactions.py
+++ bot/models/transactions.py
@@ -1,6 +1,7 @@
 from sqlalchemy import Column, Integer, Float, String, DateTime, ForeignKey
 from sqlalchemy.orm import relationship
 from bot.database.database import Base
+from datetime import datetime
 
 
 class Transactions(Base):
@@ -8,11 +9,18 @@ class Transactions(Base):
     Модель транзакций пользователей
     """
     __tablename__ = 'transactions'
+
     id = Column(Integer, primary_key=True, autoincrement=True)
+    user_tg_id = Column(Integer, ForeignKey('users.user_tg_id', ondelete='CASCADE'),
+                        nullable=False)
     amount = Column(Float, nullable=False)
-    category = Column(String, nullable=False)
-    type = Column(String, nullable=False)
-    date = Column(DateTime, nullable=False)
-    user_id = Column(Integer, ForeignKey('users.user_tg_id'))  # Изменено на user_tg_id
-    price = Column(Float, nullable=False)
+    category = Column(String(100), nullable=False)
+    type = Column(String(10), nullable=False)  # 'income' или 'expense'
+    description = Column(String(255),
+                         nullable=True)
+    date = Column(DateTime, nullable=False, default=datetime.now)
+
     user = relationship('User', back_populates='transactions')
+
+    def __repr__(self):
+        return f"<Transaction(id={self.id}, amount={self.amount}, category={self.category}, type={self.type})>"
