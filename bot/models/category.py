e6df635f03c2adb9bf9a25d8f127b48f1698f281
Wlwool <blackenedima@yandex.ru>
2025-08-10 13:00:50 +0300

    изменения в связях, исправления в id

--- bot/models/category.py
+++ bot/models/category.py
@@ -9,10 +9,15 @@ class Category(Base):
     Модель таблицы категории расходов
     """
     __tablename__ = 'categories'
-    id = Column(Integer, primary_key=True)
-    user_id = Column(Integer, ForeignKey('users.id'))
+
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    user_tg_id = Column(Integer, ForeignKey('users.user_tg_id', ondelete='CASCADE'),
+                        nullable=False)
     name = Column(String(50), nullable=False)
     type = Column(String(10), nullable=False)  # 'income' или 'expense'
     created_at = Column(DateTime, default=datetime.now)
 
     user = relationship("User", back_populates="categories")
+
+    def __repr__(self):
+        return f"<Category(id={self.id}, name={self.name}, type={self.type})>"
