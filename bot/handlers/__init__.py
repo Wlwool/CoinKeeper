8f9cd30bae90d186f1023a90d61bcf222306ba33
Wlwool <blackenedima@yandex.ru>
2025-08-11 11:48:44 +0300

    долбавлен роутер для удаления записей

--- bot/handlers/__init__.py
+++ bot/handlers/__init__.py
@@ -3,6 +3,7 @@ from .cancel_handler import router as cancel_router
 from .basic_commands import router as basic_router
 from .transactions import router as transactions_router
 from .transaction_history import router as history_router
+from .delete_transaction import router as delete_router
 from .unknown_handler import router as unknown_router
 
 main_router = Router()
@@ -11,6 +12,7 @@ main_router.include_router(cancel_router)
 main_router.include_router(basic_router)
 main_router.include_router(transactions_router)
 main_router.include_router(history_router)
+main_router.include_router(delete_router)
 main_router.include_router(unknown_router)
 
 __all__ = ["main_router"]
